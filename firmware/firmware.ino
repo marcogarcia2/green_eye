0bc49bf7b6f96164e26f1114d60e951b198947c2
Marco Garcia <mmgaspargarcia@gmail.com>
2025-05-29 11:32:08 -0300

    Protótipo do código

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -14,15 +14,28 @@ void blueLED(bool status){
 void setup(){
 
   Serial.begin(115200);
-  connectWiFi();
+
   blueLED(true);
+  // Ler dados dos sensores
+  //
 
+  // Escrever os dados:
+  connectWiFi();
+  /// Descobrir que dia é hoje
+  /// Descobrir que horas são (hora que começou) (talvez usar variavel estatica)
+
+
+  // Conectar com o banco de dados
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-
-  Serial.println();
-  Serial.print(Firebase.getString("/Estufa_teste/name"));
-  Serial.println();
+  
+  // Escrever dados
+  /// Criar função que escreve cada tipo de dado no dia e horário atual
   blueLED(false);
+  
+  // Calcular quanto deve-se dormir
+  /// Baseado no horário atual (Exato)
+  //deepSleep(tempo);
+
 }
 
 
